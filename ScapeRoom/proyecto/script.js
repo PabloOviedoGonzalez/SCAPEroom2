eb85e4875acdc2b47d0f0e55bb9955174f90d155
Daniel Salguero Maqueda <danielsalguero15@gmail.com>
2026-03-02 14:13:02 +0100

    Cambiados triggers y textos de las escenas del camino 2 (Mewtwo)

--- ScapeRoom/proyecto/script.js
+++ ScapeRoom/proyecto/script.js
@@ -3,24 +3,24 @@
 const escenas = [
     {//0
         imagen: "img/entrada.png",
-        texto: "Estás en la habitación. Busca algo sospechoso.",
+        texto: "Te encuentras en la calle, frente a la entrada de una cueva oscura. El aire que sale del interior es frío y extraño. Parece que algo se mueve dentro… ¿Te atreves a entrar?",
         triggers: [
-            { top: 220, left: 300, width: 80, height: 80, next: 1 }
+            { top: 43, left: 44, width: 13, height: 32, next: 1 }
         ]
     },
 
     {//1
         imagen: "img/escena2puertas.png",
-        texto: "Has encontrado dos caminos. ¿Cuál eliges?",
+        texto: "Has entrado en la cueva. La luz del exterior desaparece poco a poco y todo se vuelve más silencioso. Solo escuchas gotas de agua cayendo desde el techo. Frente a ti hay unas escaleras que bajan a la oscuridad.",
         triggers: [
-            { top: 180, left: 200, width: 100, height: 100, next: 2 },
-            { top: 180, left: 500, width: 100, height: 100, next: 3 }
+            { top: 18, left: 76, width: 8, height: 20, next: 2 },
+            { top: 38, left: 30, width: 6, height: 12, next: 3 }
         ]
     },
 
     {//2
         imagen: "img/pokemon_oscuro.png",
-        texto: "Que pokemon me ayudará?",
+        texto: "Llegas a una sala completamente oscura. De repente, dos Pokémon aparecen ante ti. Uno parece amistoso… pero otro tiene una mirada peligrosa.",
         triggers: [
             { top: 180, left: 200, width: 100, height: 100, next: 4 },
             { top: 180, left: 500, width: 100, height: 100, next: 7 }]
@@ -28,9 +28,9 @@ const escenas = [
 
     {//3
         imagen: "img/mewtwo.png",
-        texto: "Camino derecho — FIN",
+        texto: "Un fuerte temblor sacude la cueva. De entre las sombras aparece un Pokémon legendario. Su poder bloquea la salida. ¡Captúralo!",
         triggers: [
-            { top: 220, left: 300, width: 80, height: 80, next: 10 }
+            { top: 70, left: 56, width: 15, height: 25, next: 10 }
         ]
     },
 
@@ -82,9 +82,9 @@ const escenas = [
 
     {//10
         imagen: "img/captura.png",
-        texto: "Camino derecho — FIN",
+        texto: "Has capturado al Pokémon que custodiaba la salido. ¡Ahora vuelve a ser visible!",
         triggers: [
-            { top: 220, left: 300, width: 80, height: 80, next: 6 }
+            { top: 65, left: 45, width: 15, height: 25, next: 6 }
         ]
     }
 
@@ -129,10 +129,10 @@ function cargarEscena() {
         const div = document.createElement("div");
         div.className = "trigger";
 
-        div.style.top = t.top + "px";
-        div.style.left = t.left + "px";
-        div.style.width = t.width + "px";
-        div.style.height = t.height + "px";
+        div.style.top = t.top + "%";
+        div.style.left = t.left + "%";
+        div.style.width = t.width + "%";
+        div.style.height = t.height + "%";
 
         div.addEventListener("click", () => {
             cambiarEscenaConFade(t.next);
